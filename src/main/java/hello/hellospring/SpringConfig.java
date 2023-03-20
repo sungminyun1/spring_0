964ab878f7e181879c4797cb69b944d4e3f82279
sungminyun1 <smy07207@gmail.com>
2023-03-20 21:03:45 +0900

    0320

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,23 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+    private EntityManager em;
+
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -26,7 +27,13 @@ public class SpringConfig {
 //    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //    }
+//    public MemberRepository memberRepository(){
+//        return new JdbcMemberRepository(dataSource);
+//    }
+//    public MemberRepository memberRepository(){
+//        return new JdbcTemplateMemberRepository(dataSource);
+//    }
     public MemberRepository memberRepository(){
-        return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
---
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,43 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    private final EntityManager en;
+
+    public JpaMemberRepository(EntityManager en) {
+        this.en = en;
+    }
+
+    @Override
+    public Member save(Member member) {
+        en.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = en.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return en.createQuery("select m from Member m where m.name = :name",Member.class)
+                .setParameter("name",name)
+                .getResultList()
+                .stream().findAny();
+
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return en.createQuery("select m from Member m",Member.class)
+                .getResultList();
+    }
+}
